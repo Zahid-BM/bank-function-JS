abe1136d960fcff2324a3b6669e49768ddfef8a7
BM. Zahidul Islam <bmzahidulislam@outlook.com>
2022-02-14 00:11:01 +0600

    inputTotalSum() added to deposit and withdraw success

--- js/account-summery.js
+++ js/account-summery.js
@@ -1,45 +1,46 @@
 // main website background color 
 document.body.style.backgroundColor = '#6867AC';
 
-// function for capturing deposit input value 
-function getInputValue(inputId) {
-    const input = document.getElementById(inputId);
+// function for capturing input value 
+function gettingInputValue(inputIdName) {
+    const input = document.getElementById(inputIdName);
     const inputValue = input.value;
     const inputValueToNumber = parseFloat(inputValue);
     // clear input field after clicking 
     input.value = '';
     return inputValueToNumber;
 }
+// function for capturing input total value 
+function inputTotalSum(amountidName) {
+    const totalInputAmount = document.getElementById(amountidName)
+    const totalInputAmountInnerText = totalInputAmount.innerText;
+    const totalInputAmountInnerTextNumber = parseFloat(totalInputAmountInnerText);
+    totalInputAmount.innerText = totalInputAmountInnerTextNumber;
+    return totalInputAmountInnerTextNumber;
+}
 
 // capture deposit input and show in deposite amount  
 document.getElementById('btn-deposit-input').addEventListener('click', function () {
-    /*  const depositInput = document.getElementById('input-deposit-amount');
-     const depositeInputValue = depositInput.value;
-     const depositInputValueNumber = parseFloat(depositeInputValue); */
-    const depositeInputValueFunction = getInputValue('input-deposit-amount');
-    const depositeAmount = document.getElementById('deposit-amount');
-    const depositeAmountInnerText = depositeAmount.innerText;
-    const depositAmountInnerTextNumber = parseFloat(depositeAmountInnerText);
-    const depositSum = depositAmountInnerTextNumber + depositeInputValueFunction;
-    depositeAmount.innerText = depositSum;
+    const depositeInputValueNumber = gettingInputValue('input-deposit-amount');
+    const totalDeposit = inputTotalSum('deposit-amount');
+    const depositAmount = document.getElementById('deposit-amount');
+    const depositSummery = totalDeposit + depositeInputValueNumber;
+    // console.log('Success');
+    depositAmount.innerText = depositSummery;
     // updating account balance 
     const balanceAmount = document.getElementById('balance-amount');
     const balanceAmountInnerText = balanceAmount.innerText;
     const balanceAmountInnerTextNumber = parseFloat(balanceAmountInnerText);
-    const totalBalanceAmountNumber = depositeInputValueFunction + balanceAmountInnerTextNumber;
+    const totalBalanceAmountNumber = depositeInputValueNumber + balanceAmountInnerTextNumber;
     balanceAmount.innerText = totalBalanceAmountNumber;
 })
 // withdraw amount add then deduct from balance amount 
 document.getElementById('btn-withdraw-input').addEventListener('click', function () {
-    /* const withdrawInput = document.getElementById('input-withdraw-amount');
-    const withdrawInputValue = withdrawInput.value;
-    const withdrawInputValueNumber = parseFloat(withdrawInputValue); */
-    const withdarwInputValueNumber = getInputValue('input-withdraw-amount');
+    const withdarwInputValueNumber = gettingInputValue('input-withdraw-amount');
+    const totalwithdraw = inputTotalSum('withdarw-amount');
+    const withdrawSummery = totalwithdraw + withdarwInputValueNumber;
     const withdrawInputAmount = document.getElementById('withdarw-amount');
-    const withdrawInputAmountInnerText = withdrawInputAmount.innerText;
-    const withdrawInputAmountInnerTextNumber = parseFloat(withdrawInputAmountInnerText);
-    const withdrawInputAmountInnerTextNumberTotal = withdarwInputValueNumber + withdrawInputAmountInnerTextNumber;
-    withdrawInputAmount.innerText = withdrawInputAmountInnerTextNumberTotal;
+    withdrawInputAmount.innerText = withdrawSummery;
     // deduct from balance 
     const balanceAmount = document.getElementById('balance-amount');
     const balanceAmountInnerText = balanceAmount.innerText;
