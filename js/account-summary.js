538f5c4e70eeb3ae3fd9a72c4ab6ed3bf1b2ff1a
BM. Zahidul Islam <bmzahidulislam@outlook.com>
2022-02-14 17:07:27 +0600

    project done

--- js/account-summary.js
+++ js/account-summary.js
@@ -7,11 +7,20 @@ deposite event handler : capture deposit input and show in deposite amount
 ##########################################################################################
 #################################################################################### */
 document.getElementById('btn-deposit-input').addEventListener('click', function () {
-    /*  show deposit total  */
+
     const inputValueNumber = gettingInputValueNumber('input-deposit');
-    gettingInputTotalSum('deposit-amount', inputValueNumber);
-    /* updating account balance  */
-    adjustingBalance(inputValueNumber, true);
+
+    if (inputValueNumber > 0) {
+        /*  show deposit total  */
+        gettingInputTotalSum('deposit-amount', inputValueNumber);
+        /* updating account balance  */
+        adjustingBalance(inputValueNumber, true);
+    }
+    else {
+        alert('You have entered wrong value. Plz input correctly');
+    }
+
+
 })
 /* ##########################################################################################
 ####################################################################################
@@ -19,11 +28,18 @@ withdraw event handler: withdraw amount add then deduct from balance amount
 ##########################################################################################
 #################################################################################### */
 document.getElementById('btn-withdraw-input').addEventListener('click', function () {
-    /*  show withdraw total  */
     const inputValueNumber = gettingInputValueNumber('input-withdraw');
-    gettingInputTotalSum('withdarw-amount', inputValueNumber);
-    /*  deduct from balance  */
-    adjustingBalance(inputValueNumber, false);
+    const currentBalanceAmount = currentBalance();
+
+    if (inputValueNumber > 0 && inputValueNumber < currentBalanceAmount) {
+        /*  show withdraw total  */
+        gettingInputTotalSum('withdarw-amount', inputValueNumber);
+        /*  deduct from balance  */
+        adjustingBalance(inputValueNumber, false);
+    }
+    else {
+        alert('You have entered wrong value. Plz input correctly');
+    }
 
 })
 /* ##########################################################################################
@@ -51,15 +67,21 @@ function gettingInputTotalSum(amountId, inputValueNumber) {
 // function for adjusting balance amount 
 function adjustingBalance(inputValueNumber, isAdd) {
     const balanceAmount = document.getElementById('balance-amount');
-    const balanceAmountInnerText = balanceAmount.innerText;
-    const balanceAmountInnerTextNumber = parseFloat(balanceAmountInnerText);
+    const currentBalanceAmount = currentBalance();
     if (isAdd == true) {
-        const totalBalanceAmountNumber = balanceAmountInnerTextNumber + inputValueNumber;
+        const totalBalanceAmountNumber = currentBalanceAmount + inputValueNumber;
         balanceAmount.innerText = totalBalanceAmountNumber;
     }
     else {
-        const totalBalanceAmountNumber = balanceAmountInnerTextNumber - inputValueNumber;
+        const totalBalanceAmountNumber = currentBalanceAmount - inputValueNumber;
         balanceAmount.innerText = totalBalanceAmountNumber;
     }
+}
+// function for current balance 
+function currentBalance() {
+    const findBalanceAmount = document.getElementById('balance-amount');
+    const balanceAmountInnerText = findBalanceAmount.innerText;
+    const balanceAmountInnerTextNumber = parseFloat(balanceAmountInnerText);
+    return balanceAmountInnerTextNumber;
 }
 
